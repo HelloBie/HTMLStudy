e999ea681c11bfa55234c21e1ff034524c490433
bieqiutian <1005903848@qq.com>
2023-12-20 16:53:43 +0800

    vue脚手架

---
+++ vue脚手架/vuejs/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div>
+       <schoolView/>
+    <schoolView/>
+    </div>
+  
+</template>
+
+<script>
+import schoolView from './common/schoolView'
+export default {
+    name:'App',
+components:{schoolView}
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue脚手架/vuejs/src/common/schoolView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bgd">
+    <h2>name:{{name}}</h2>
+    <h2>address:{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  
+    data() {
+        return {
+            name: 'hellBie',
+            address: '长春'
+        }
+    },
+}
+</script>
+
+<style>
+.bgd {
+    background-color: gray;
+}
+</style>
